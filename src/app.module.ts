7bfda8b55ea59357f17d0f8750a9fd11718e7b75
vlad <vlad.bras02@gmail.com>
2023-05-15 15:16:25 +0200

    feat: configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,26 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql')
-  }), BooksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }), BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,10 +1,19 @@
 import { Field, ID, ObjectType } from "@nestjs/graphql"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
+@Entity('books')
 @ObjectType({ description: 'Book model' })
 export class Book {
+    @PrimaryGeneratedColumn()
     @Field(() => ID, { description: 'A unique identifier' })
     id: number
+
+    @Column()
     title: string
+
+    @Column()
     price: number
+
+    @Column()
     author: string
 }
