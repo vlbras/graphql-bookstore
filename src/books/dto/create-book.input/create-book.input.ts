a2626841aae360335c5be44cb4b043150572ed57
vlad <vlad.bras02@gmail.com>
2023-05-16 10:47:08 +0200

    fix: remove unnecessary files

--- src/books/dto/create-book.input/create-book.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from "@nestjs/graphql";
-
-@InputType({ description: 'Create book input object type'})
-export class CreateBookInput {
-    @Field(()=> String, {description: 'A new book title'})
-    title: string
-    price: number
-    author: string
-}
