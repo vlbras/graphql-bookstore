a723704cef20917fced03e4557c58a31885c3fa8
vlad <vlad.bras02@gmail.com>
2023-05-16 11:41:27 +0200

    feat: add book validation

--- src/books/dto/create-book.input.ts
+++ src/books/dto/create-book.input.ts
@@ -1,9 +1,14 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsInt, IsString, MinLength } from "class-validator";
 
 @InputType({ description: 'Create book input object type'})
 export class CreateBookInput {
+    @IsString()
+    @MinLength(3)
     @Field(()=> String, {description: 'A new book title'})
     title: string
+    @IsInt()
     price: number
+    @IsString()
     author: string
 }
