51e014ba03c86eb4d78ad044568e9f7dcd470f18
vlad <vlad.bras02@gmail.com>
2023-05-16 10:42:56 +0200

    feat: add book crud operations

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -1,26 +1,40 @@
 import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Book } from './entities/book.entity';
 import { ParseIntPipe } from '@nestjs/common';
-import { CreateBookInput } from './dto/create-book.input/create-book.input';
+import { CreateBookInput } from './dto/create-book.input';
 import { BooksService } from './books.service';
+import { UpdateBookInput } from './dto/update-book.input';
 
 @Resolver()
 export class BooksResolver {
-    constructor(private readonly booksService: BooksService){}
+    constructor(private readonly booksService: BooksService) { }
 
     @Query(() => [Book], { name: 'books' })
     async findAll() {
         return this.booksService.findAll()
     }
 
-    @Query(() => Book, { name: 'book', nullable: true })
+    @Query(() => Book, { name: 'book' })
     async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
         return this.booksService.findOne(id)
     }
 
-    @Mutation(() => Book, { name: 'createBook', nullable: true })
+    @Mutation(() => Book, { name: 'createBook' })
     async create(
         @Args('createBookInput') createBookInput: CreateBookInput) {
         return this.booksService.create(createBookInput)
+    }
+
+    @Mutation(() => Book, { name: 'updateBook' })
+    async update(
+        @Args('id', ParseIntPipe) id: number,
+        @Args('updateBookInput') updateBookInput: UpdateBookInput,
+    ) {
+        return this.booksService.update(id, updateBookInput)
+    }
+
+    @Mutation(()=> Book, {name: 'removeBook'})
+    async remove(@Args('id', ParseIntPipe) id: number){
+        return this.booksService.remove(id)
     }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBookInput } from './dto/create-book.input/create-book.input';
+import { CreateBookInput } from './dto/create-book.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
+import { UserInputError } from 'apollo-server-express';
+import { UpdateBookInput } from './dto/update-book.input';
 
 @Injectable()
 export class BooksService {
-    async findAll(){
-        return []
+    constructor(
+        @InjectRepository(Book)
+        private readonly bookRepository: Repository<Book>
+    ) { }
+
+    async findAll(): Promise<Book[]> {
+        return this.bookRepository.find()
     }
 
-    async findOne(id: number){
-        return null
+    async findOne(id: number): Promise<Book>  {
+        const book = await this.bookRepository.findOneBy({ id })
+        if (!book) {
+            throw new UserInputError(`Book #${id} not found`)
+        }
+        return book
     }
 
-    async create(createBookInput: CreateBookInput){
-        return null
+    async create(createBookInput: CreateBookInput): Promise<Book> {
+        const book = this.bookRepository.create(createBookInput)
+        return this.bookRepository.save(book)
+    }
+
+    async update(id: number, updateBookInput: UpdateBookInput): Promise<Book> {
+        await this.findOne(id)
+        const book = await this.bookRepository.preload({
+            id,
+            ...updateBookInput
+        })
+        return this.bookRepository.save(book)
+    }
+
+    async remove(id: number): Promise<Book>{
+        const book = await this.findOne(id)
+        return this.bookRepository.remove(book)
     }
 }
